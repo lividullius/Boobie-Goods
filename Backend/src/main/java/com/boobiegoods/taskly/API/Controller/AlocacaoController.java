b99deaa591383dc6b5a34764d196260865fc99ec
Marcela Nunes Zarichta <marcela.zarichta@edu.pucrs.br>
2025-09-23 19:12:25 -0300

    adaptacao do codigo para banco H2

--- Backend/src/main/java/com/boobiegoods/taskly/API/Controller/AlocacaoController.java
+++ Backend/src/main/java/com/boobiegoods/taskly/API/Controller/AlocacaoController.java
@@ -213,7 +213,7 @@ public class AlocacaoController {
         contrato1.setDataInicioContrato(LocalDate.of(2024, 1, 1));
         contrato1.setDataFimContrato(LocalDate.of(2024, 12, 31));
         contrato1.setNumeroHorasSemana(40);
-        contrato1.setValorHora(50.0);
+        contrato1.setValorHora(50);
         contratos.add(contrato1);
         
         Contrato contrato2 = new Contrato();
@@ -222,7 +222,7 @@ public class AlocacaoController {
         contrato2.setDataInicioContrato(LocalDate.of(2024, 3, 1));
         contrato2.setDataFimContrato(LocalDate.of(2025, 2, 28));
         contrato2.setNumeroHorasSemana(30);
-        contrato2.setValorHora(45.0);
+        contrato2.setValorHora(45);
         contratos.add(contrato2);
         
         // Criar alocações de exemplo
